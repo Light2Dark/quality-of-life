8e378d6522038d35d002805d0a26e738ba609a46
Sham9871@gmail.com <Sham9871@gmail.com>
2023-04-06 04:18:28 +0000

    initial dbt jaffle_shop learning

---
+++ dbt/models/stg_customers.sql
@@ -0,0 +1,5 @@
+select 
+    id as customer_id,
+    first_name,
+    last_name
+from `dbt-tutorial`.jaffle_shop.customers
---
+++ dbt/models/stg_orders.sql
@@ -0,0 +1,6 @@
+select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+from `dbt-tutorial`.jaffle_shop.orders
