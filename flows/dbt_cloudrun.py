e2492f706976e43848b742e33b19e74ba8da837a
Shahmir <sham9871@gmail.com>
2023-04-17 19:16:47 +0800

    added prefect dbt integration

--- flows/dbt_cloudrun.py
+++ flows/dbt_cloudrun.py
@@ -1,52 +1,38 @@
-from prefect_dbt.cloud.jobs import DbtCloudJob, run_dbt_cloud_job
-from prefect_dbt.cloud import DbtCloudCredentials
-from prefect import flow
+from elt_web_to_bq import elt_flow
 
+from prefect_github import GitHubCredentials
+github_credentials_block = GitHubCredentials.load("github-credentials")
 
-from prefect.flows import healthcheck
-from prefect import get_client
-from prefect.deployments import Deployment
-from prefect.filesystems import GCS
-from prefect_gcp.cloud_run import CloudRunJob
+# from prefect import get_client
+# client = get_client()
 
-client = get_client()
-
-gcs_block = GCS.load("my-gcs-bucket")
-cloud_run_job_block = CloudRunJob.load("my-gcp-infrastructure")
-
-
-deployment = Deployment.build_from_flow(
-    flow=healthcheck,
-    name="GCP test flow",
-    storage=gcs_block,
-    infrastructure=cloud_run_job_block,
-)
-
-if __name__ == "__main__":
-    deployment.apply()
-
-development_run_jobid = 264248
-
-# dbt_cloud_credentials = DbtCloudCredentials.load("dbt-cloud")
-# dbt_cloud_job = DbtCloudJob(
-#     dbt_cloud_credentials=dbt_cloud_credentials,
-#     job_id=development_run_jobid
-# ).save("development-run-job")
-
-@flow
 def run_dbt_job_flow():
+    from prefect_dbt.cloud.jobs import DbtCloudJob, run_dbt_cloud_job
+    from prefect_dbt.cloud import DbtCloudCredentials
+    
+    development_run_jobid = 264248
+    
+    dbt_cloud_credentials = DbtCloudCredentials.load("dbt-cloud")
+    dbt_cloud_job = DbtCloudJob(
+        dbt_cloud_credentials=dbt_cloud_credentials,
+        job_id=development_run_jobid
+    ).save("development-run-job")
+    
     result = run_dbt_cloud_job(
         dbt_cloud_job=DbtCloudJob.load("development-run-job"),
         targeted_retries=2,
     )
     print(result)
 
-# run_dbt_job_flow()
-
-import pkg_resources
-from subprocess import call
 
 def update_pip_packages():
+    import pkg_resources
+    from subprocess import call
     packages = [dist.project_name for dist in pkg_resources.working_set]
-
     call("pip install --upgrade " + ' '.join(packages), shell=True)
+    
+    
+    
+if __name__ == "__main__":
+    # deployment.apply()
+    pass
