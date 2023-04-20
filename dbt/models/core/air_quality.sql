006dd83e8a1df16859f86c445ce78441864ff472
Sham9871@gmail.com <Sham9871@gmail.com>
2023-04-20 02:51:25 +0000

    dbt ready for prod

--- dbt/models/core/air_quality.sql
+++ dbt/models/core/air_quality.sql
@@ -8,6 +8,6 @@ select
     unit
 from {{ref('stg_hourly_air_quality')}}
 left join {{ref('air_quality_indicators')}} ON pollutant_symbol = symbol
-{% if var('is_test_run', default=true) %}
+{% if var('test_run', default=true) %}
   limit 100
 {% endif %}
