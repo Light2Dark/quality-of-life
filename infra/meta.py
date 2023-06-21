4f60013180f9928a93e1509beb0e912e1ed589e7
Shahmir <sham9871@gmail.com>
2023-06-21 19:09:20 +0800

    utilities

---
+++ infra/meta.py
@@ -0,0 +1,7 @@
+def upgrade_packages():
+    """Upgrade all packages in the current virtual environment."""
+    import pkg_resources
+    from subprocess import call
+
+    for dist in pkg_resources.working_set:
+        call("python -m pip install --upgrade " + dist.project_name, shell=True)
